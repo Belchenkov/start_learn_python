42e1878246dc12af61bfd6da947db067f229b3ac
Aleksey Belchenkov <u608110@gmail.com>
2019-11-04 09:04:26 +0300

    String - line break

---
+++ lesson8.py
@@ -0,0 +1,27 @@
+words = 'Hello, "some" world!'
+words = "Hello, 'some' world 2!"
+words = "Hello, \"some\" world 23!"
+words = 'Hello, \'some\' world 4!'
+words = 'Hello, \\some\' world 52!'
+
+verse = 'sdfsdfsd\
+        sdfsdfsdf\
+        sdfsdfsdf\
+        sdfsdfsdfsdf'
+
+verse = 'sdfsdfsd\n\
+        sdfsdfsdf\n\
+        sdfsdfsdf\n\
+        sdfsdfsdfsdf'
+
+verse = ('sdfsdfsd\n'
+        'sdfsdfsdf\n'
+        'sdfsdfsdfsdf')
+
+verse = '''\
+        'sdfsdfsd'
+        "sdfsdfsdf"
+        sdfsdfsdfsdf\
+        '''
+
+print(verse)
