224d0d825bf9326892a66672acf669fe13b3da0b
Aleksey Belchenkov <u608110@gmail.com>
2019-11-09 07:24:00 +0300

    Operation with strings

---
+++ lesson9.py
@@ -0,0 +1,25 @@
+s = r'C:\d\new.txt'
+s = 'Py' 'thon'
+
+s1 = 'Hello'
+s2 = 'world!'
+s3 = s1 + ' ' + s2
+
+name = 'John'
+age = 20
+
+#print('My name is ' + name + ". I'm " + str(age))
+#print('hi' * 5)
+
+s = 'Hello world!'
+# print(s[8])
+# print(s[-1])
+# s[0] = 'D' - error
+
+# print(s[0:12])
+# print(s[0:5])
+# print(s[:5])
+# print(s[6:])
+# print(s[::2])
+# print(s[::-1])
+print(s[:5] + s[6:])
