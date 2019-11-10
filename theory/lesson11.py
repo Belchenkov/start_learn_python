defac75c4d2f606bba9e61c491e595c016560a69
Aleksey Belchenkov <u608110@gmail.com>
2019-11-10 09:34:15 +0300

    String format

---
+++ theory/lesson11.py
@@ -0,0 +1,14 @@
+name = 'John'
+age = 31
+
+# print('My name is ' + name + '. I am ' + str(age))
+# print('My name is %(name)s. I am %(age)d' %{'name': name, 'age': age})
+# print('My name is %s. I am %d' %(name, age))
+# print('My name is %s. I am %d' %(name, age))
+# print('Title %s, Price %.2f' %('Sony', 40))
+
+# print('My name is {}. I am {}'.format(name, age))
+# print('My name is {0}. I am {1}'.format(age, name))
+# print('My name is {name}. I am {age}'.format(name='Andrey', age=age))
+# print(f'My name is {name}. I am {age + 4}')
+print(f'5 + 2 = {5 + 2}')
