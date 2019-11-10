bc177f669a55b0d03be43b83706051694648e536
Aleksey Belchenkov <u608110@gmail.com>
2019-11-10 08:27:58 +0300

    String functions

---
+++ lesson10.py
@@ -0,0 +1,17 @@
+s = 'hello'
+s = 'Привет'
+# print(len(s))
+
+s = 'HELLO'
+# print(s.capitalize())
+
+s = 'hello world'
+# print(s.center(15, '#'))
+# print(s.count('l'))
+# print(s.find('lo'))
+# print(s.index('a'))
+# print(s.replace('l', 't'))
+# print(s.split('l'))
+# print(s.isdigit())
+print(s.isalpha())
+
