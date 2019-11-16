99c90f46aa0c8c0e0f926ceb67b3f67f90a0dda5
Aleksey Belchenkov <u608110@gmail.com>
2019-11-16 10:24:26 +0300

    Operation If

---
+++ theory/lesson12.py
@@ -0,0 +1,44 @@
+# x = 10
+#
+# if x:
+#     print('x is true')
+# else:
+#     print('x is false')
+
+# light = 'green'
+#
+# if light == 'red':
+#     print('Stop')
+# elif light == 'yellow':
+#     print('Wait')
+# elif light == 'green':
+#     print('Go')
+# else:
+#     print('What?')
+
+# age = int(input('How old are you?'))
+#
+# if age >= 18:
+#     print('Welcome!')
+# else:
+#     print(f'You are {age}. Need {18 - age}')
+#
+# time = 12
+# day = 'st'
+#
+# if time > 8 and day != 'su':
+#     print('Open')
+# else:
+#     print('Close')
+
+# x = 1
+# if not x:
+#     print('True')
+# else:
+#     print('False')
+
+x = 0
+
+res = 'OK' if x else 'NO'
+
+print(res)
